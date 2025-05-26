0b6802265a7db08d862e89a5afc0be1782afebb8
sathishsk25 <sathishuma770@gmail.com>
2025-05-26 10:49:21 +0530

    Implemented Router

---
+++ src/AboutPage.jsx
@@ -0,0 +1,11 @@
+function AboutPage() {
+    return(
+        <div>
+            <h2>About our Task Manager</h2>
+            <p>This is a simple React application built to demonstrate core React concepts step-by-step.</p>
+            <p>Developed to help you learn and implement React effectively!</p>
+        </div>
+    )
+}
+
+export default AboutPage;
--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import './App.css'
 import TaskList from './components/TaskList'
 import TaskForm from './components/TaskForm'
+import AboutPage from './AboutPage'
 import { useEffect } from 'react'
 import { TaskProvider, useTask } from './TaskContext'
+import { BrowserRouter, Link, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <TaskProvider>
-      <TaskAppContent />
-    </TaskProvider>
+    <BrowserRouter>
+      <TaskProvider>
+        <TaskAppContent />
+      </TaskProvider>
+    </BrowserRouter>
   )
 }
 
@@ -23,8 +27,24 @@ function TaskAppContent() {
 
   return (
     <div style={{ fontFamily: 'Arial, sans-serif', maxWidth: '600px', margin: '20px auto', padding: '20px', border: '1px solid #eee', boxShadow: '2px 2px 5px rgba(0,0,0,0.1)' }}>
-      <TaskList />
-      <TaskForm />
+      <h1>Task Manager</h1>
+      
+      <nav style={{ marginBottom: '20px', borderBottom: '1px solid #ddd', paddingBottom: '10px' }}>
+        <Link to="/" style={{ marginRight: '15px', textDecoration: 'none', color: '#007bff', fontWeight: 'bold' }}>Home</Link>
+        <Link to="/about" style={{ textDecoration: 'none', color: '#007bff', fontWeight: 'bold' }}>About</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element= {
+          <>          
+            <TaskList />
+            <TaskForm />
+          </>
+        }/>
+        <Route path='/about' element={<AboutPage />} />
+      </Routes>
+
+      
     </div>
   );
 }
